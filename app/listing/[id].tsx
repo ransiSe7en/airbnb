0fd915bd970999f48e0fe71a2c02c709c7bc2003
ransiSe7en <ransihowler@gmail.com>
2024-05-31 01:16:56 +0530

    code

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,14 +1,175 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, useNavigation } from 'expo-router';
+import React, { useLayoutEffect } from 'react';
+import { View, Text, StyleSheet, Image, Dimensions, TouchableOpacity, Share } from 'react-native';
+import listingsData from '@/assets/data/air-bnb-listings.json';
+// import {Listing} from '@interfaces/listing';
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
+import Animated, {
+  SlideInDown,
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+} from 'react-native-reanimated';
+import { defaultStyles } from '@/constants/styles';
 
+const IMG_HEIGHT = 300;
+const {width} = Dimensions.get('window');
 const Page = () => {
   const {id} = useLocalSearchParams<{id:string}>();
+  const listing: Listing = (listingsData as any[]).find((item) => item.id === id);
+  const scrollRef = useAnimatedRef<AnimatedScrollView>();
+
+  const scrollOffset = useScrollViewOffset(scrollRef)
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      <Animated.ScrollView
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ref={scrollRef}      
+        scrollEventThrottle={16}>
+        <Animated.Image
+          source={{ uri: listing.xl_picture_url }}
+          style={[styles.image, imageAnimatedStyle]}
+          resizeMode="cover"
+        />
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.name}>{listing.name}</Text>
+          <Text style={styles.location}>
+            {listing.room_type} in {listing.smart_location}
+          </Text>
+          <Text style={styles.rooms}>
+            {listing.guests_included} guests · {listing.bedrooms} bedrooms · {listing.beds} bed ·{' '}
+            {listing.bathrooms} bathrooms
+          </Text>
+          <View style={{ flexDirection: 'row', gap: 4 }}>
+            <Ionicons name="star" size={16} />
+            <Text style={styles.ratings}>
+              {listing.review_scores_rating / 20} · {listing.number_of_reviews} reviews
+            </Text>
+          </View>
+          <View style={styles.divider} />
+
+          <View style={styles.hostView}>
+            <Image source={{ uri: listing.host_picture_url }} style={styles.host} />
+
+            <View>
+              <Text style={{ fontWeight: '500', fontSize: 16 }}>Hosted by {listing.host_name}</Text>
+              <Text>Host since {listing.host_since}</Text>
+            </View>
+          </View>
+
+          <View style={styles.divider} />
+
+          <Text style={styles.description}>{listing.description}</Text>
+        </View>
+      </Animated.ScrollView>
+
+      <Animated.View style={defaultStyles.footer} entering={SlideInDown.delay(200)}>
+        <View
+          style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+          <TouchableOpacity style={styles.footerText}>
+            <Text style={styles.footerPrice}>€{listing.price}</Text>
+            <Text>night</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[defaultStyles.btn, { paddingRight: 20, paddingLeft: 20 }]}>
+            <Text style={defaultStyles.btnText}>Reserve</Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  image: {
+    height: IMG_HEIGHT,
+    width: width,
+  },
+  infoContainer: {
+    padding: 24,
+    backgroundColor: '#fff',
+  },
+  name: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    fontFamily: 'mon-sb',
+  },
+  location: {
+    fontSize: 18,
+    marginTop: 10,
+    fontFamily: 'mon-sb',
+  },
+  rooms: {
+    fontSize: 16,
+    color: Colors.grey,
+    marginVertical: 4,
+    fontFamily: 'mon',
+  },
+  ratings: {
+    fontSize: 16,
+    fontFamily: 'mon-sb',
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: Colors.grey,
+    marginVertical: 16,
+  },
+  host: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
+  },
+  hostView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+  },
+  footerText: {
+    height: '100%',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+  },
+  footerPrice: {
+    fontSize: 18,
+    fontFamily: 'mon-sb',
+  },
+  roundButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: Colors.primary,
+  },
+  bar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+  },
+  header: {
+    backgroundColor: '#fff',
+    height: 100,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.grey,
+  },
+
+  description: {
+    fontSize: 16,
+    marginTop: 10,
+    fontFamily: 'mon',
+  },
+});
 
 export default Page
--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,9 +1,11 @@
-import { View, Text, ListRenderItem } from 'react-native'
+import { View, Text, StyleSheet, ListRenderItem, TouchableOpacity, ViewBase ,Image} from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import { defaultStyles } from '@/constants/styles';
 // import { FlatList } from 'react-native-reanimated/lib/typescript/Animated';
 import { FlatList } from 'react-native';
 import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import Animated, { FadeInRight, FadeOutLeft }  from 'react-native-reanimated';
 
 interface Props {
     listings : any[];
@@ -23,7 +25,32 @@ const Listings = ({listings: items,category }: Props) => {
 
   const renderRow: ListRenderItem<any> = ({ item }) => (
     <Link href={`/listing/${item.id}`} >
-      <Text>Go there</Text>
+      <TouchableOpacity>
+        <Animated.View style={styles.listing} entering={FadeInRight} exiting={FadeOutLeft}>
+
+          <Image source={{ uri: item.medium_url}} style={styles.image} />
+          <TouchableOpacity style={{position:'absolute', right: 30, top: 30}}>
+            <Ionicons name='heart-outline' size={24} color={'#000'} />
+          </TouchableOpacity>
+
+          <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+            <Text style={{fontSize:16, fontFamily:'mon-sb'}}>{item.name}</Text>
+            <View style={{flexDirection:'row', gap:4}}>
+              <Ionicons name='star' size={16}/>
+              <Text style={{fontFamily:'mon-sb'}}>{item.review_scores_rating /20}</Text>
+            </View>
+          </View>
+
+          <Text style={{fontFamily:'mon'}}>{item.room_type}</Text>
+
+          <View style={{flexDirection:'row',gap:4}}>  
+            <Text style={{fontFamily:'mon-sb'}}>$ {item.price}</Text>
+            <Text style={{fontFamily:'mon'}}>night</Text>
+          </View>
+          
+
+        </Animated.View>
+      </TouchableOpacity>
     </Link>
   )
 
@@ -35,6 +62,25 @@ const Listings = ({listings: items,category }: Props) => {
       renderItem={renderRow} />
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+    gap: 10,
+    marginVertical: 16,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    borderRadius: 10,
+  },
+  info: {
+    textAlign: 'center',
+    fontFamily: 'mon-sb',
+    fontSize: 16,
+    marginTop: 4,
+  },
+});
 
 export default Listings;
